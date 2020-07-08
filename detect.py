a2835b3a9ec2403b663d0b170160d5e04684bf26
noopy <young_ahn@yonsei.ac.kr>
2020-07-08 13:38:46 +0900

    testing with album image

--- detect.py
+++ detect.py
@@ -1,5 +1,5 @@
 import easyocr
 
 reader = easyocr.Reader(["en", "ko"], gpu=False)
-text = reader.readtext("./images/YBIGTA.jpg")
+text = reader.readtext("./images/R-540832-1331752044.jpeg.jpg")
 print(text)
